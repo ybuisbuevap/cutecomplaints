8f60271c7a4a0560b6510dfe9b370394b055c692
Devang Singh <prakharsingh30072@gmail.com>
2025-05-20 16:37:26 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,14 @@
-// Firebase configuration using compat SDK for your project
+// firebase-config.js
 const firebaseConfig = {
   apiKey: "AIzaSyCJm8WwAnBD2Q05rAmZB5DNDyCp87Ev3CI",
   authDomain: "cute-website.firebaseapp.com",
   databaseURL: "https://cute-website-default-rtdb.firebaseio.com",
   projectId: "cute-website",
-  storageBucket: "cute-website.firebasestorage.app",
+  storageBucket: "cute-website.appspot.com",
   messagingSenderId: "60870443586",
   appId: "1:60870443586:web:9a7eeb318133a2b4ee63db",
   measurementId: "G-YZMCZM60V7"
 };
 
-// Initialize Firebase using compat SDK
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
