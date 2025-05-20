7f6547c608f88e15e8a29c8f591d4696c9f08de5
Devang Singh <prakharsingh30072@gmail.com>
2025-05-20 20:27:18 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,16 @@
-// firebase-config.js
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCJm8WwAnBD2Q05rAmZB5DNDyCp87Ev3CI",
   authDomain: "cute-website.firebaseapp.com",
   databaseURL: "https://cute-website-default-rtdb.firebaseio.com",
   projectId: "cute-website",
-  storageBucket: "cute-website.firebasestorage.app",
+  storageBucket: "cute-website.appspot.com",
   messagingSenderId: "60870443586",
   appId: "1:60870443586:web:9a7eeb318133a2b4ee63db",
   measurementId: "G-YZMCZM60V7"
 };
+
+// ✅ THIS was missing:
+firebase.initializeApp(firebaseConfig);
 
 
